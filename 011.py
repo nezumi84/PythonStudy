b5887271bfc0dee7e64db395d50cfa74fdf9cc9d
HyeonChung Kim <khc_2007@naver.com>
2019-03-10 11:53:18 +0900

    파이썬스터디 3회차

--- 011.py
+++ 011.py
@@ -1,3 +1,14 @@
+x=1
+y=2
+if x>y:
+    print('x가 y보다 큽니다')
+elif x<y:
+    print('x가 y보다 작습니다.')
+else:
+    print('x와 y가 같습니다.')
+
+
+
 scope = [1,2,3,4,5]
 for x in scope:
     print(x)
---
+++ 033.py
@@ -0,0 +1,10 @@
+strdata1 = '나는 파이썬 프로그래머다'
+strdata2 = "you are a programmer"
+strdata3 = """I love
+    python. You love
+    python too!
+"""
+
+strdata4 = "My son's name is John"
+strdata5 = '문자열"abc"의 길이는 3 입니다.'
+
